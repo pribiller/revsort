84b4f1bc0314c248b1f15e8ee6acd62557fa492f
pribiller <pribiller@gmail.com>
2025-06-10 23:25:44 +0900

    Refactor code to make constructor more readable.

--- Tannier2007.cpp
+++ Tannier2007.cpp
@@ -12,54 +12,6 @@
 // instead of std::cout you can type directly cout.
 //using namespace std;
 
-/***********************
- * Utils
- ***********************/
-
-// Splits a vector into chunks of size approximately [chunkSize].
-std::vector<std::vector<int>> splitVector(const std::vector<int>& original, int idealChunkSize) {
-	std::vector<std::vector<int>> result;
-
-	int const numChunks	 = original.size() / idealChunkSize; // number of chunks.
-	int const baseChunkSize = original.size() / numChunks;
-	int const remainder	 = original.size() % numChunks;
-
-	// At each step creates a new sub-list.
-	int begIdx = 0;
-	for (int i=0; i<numChunks; i++) {
-		int const currentChunkSize = baseChunkSize + (i < remainder ? 1 : 0);
-		// Create a sub-vector from the current position to the next N elements.
-		std::vector<int> subVector( original.begin() + begIdx, 
-									original.begin() + begIdx + currentChunkSize);
-		// Add the new list at the end.
-		result.push_back(subVector);
-		begIdx += currentChunkSize;
-	}
-	return result;
-}
-
-// Splits a vector into chunks of size approximately [chunkSize].
-std::vector<std::list<int>> splitVectorIntoLists(const std::vector<int>& original, int idealChunkSize) {
-	std::vector<std::list<int>> result;
-
-	int const numChunks	 = original.size() / idealChunkSize; // number of chunks.
-	int const baseChunkSize = original.size() / numChunks;
-	int const remainder	 = original.size() % numChunks;
-
-	// At each step creates a new sub-list.
-	int begIdx = 0;
-	for (int i=0; i<numChunks; i++) {
-		int const currentChunkSize = baseChunkSize + (i < remainder ? 1 : 0);
-		// Create a sub-vector from the current position to the next N elements.
-		std::list<int> subList( original.begin() + begIdx, 
-								original.begin() + begIdx + currentChunkSize);
-		// Add the new list at the end.
-		result.push_back(subList);
-		begIdx += currentChunkSize;
-	}
-	return result;
-}
-
 /*******************************************************
  * Auxiliary data structures.
  *******************************************************/
@@ -68,14 +20,14 @@ class Block {
 public:
 
 	int blockId;
-	std::list<int> block;
+	std::list<int> permutationSegment;
 	bool reversed{false}; // raised if the block should be read in the reverse order, changing the sign of the elements.
 
-	Block(int id, const std::list<int>& b):blockId(id),block(b){
+	Block(int id, const std::list<int>& p):blockId(id),permutationSegment(p){
 	}
-	
+
 	void printBlock(){
-		for(auto const &g : block) {std::cout << g << " ";}
+		for(auto const &g : permutationSegment) {std::cout << g << " ";}
 		std::cout.put('\n');
 	}
 };
@@ -87,81 +39,99 @@ public:
 class GenomeSort {
 private:
 	std::list<Block> blocks;
-	int blockId_max;
 	std::vector<std::list<Block>::iterator> genes_to_blocks; // map between genes and blocks.
 	std::vector<std::list<int>::iterator> genes; // references of genes.
+	int blockId_max{0};
+	int n; // number of genes
 
-	std::list<Block>::iterator getBlock(int gene){
-		return genes_to_blocks[std::abs(gene)];
+	/* Adds a new block after the specified position. */
+	void createNewBlock(std::list<int>& permSegment, std::list<Block>::iterator position){
+		// Create a new block containing all genes from ``permSegment``.
+		// This new block will be inserted after the block given in the input.
+		std::list<Block>::iterator new_block;
+		if (position != blocks.end()) {
+			blocks.emplace(std::next(position), ++blockId_max, permSegment);
+			// Get an iterator to the new block added to the list.
+			new_block = std::next(position);
+		} else {
+			blocks.emplace_back(++blockId_max, permSegment); // It return a reference to the block (Block& b)
+			// Get an iterator to the new block added to the list.
+			new_block = std::prev(blocks.end());
+		}
+		// Updates the reference block for affected genes.
+	    for (const int& g : permSegment) {genes_to_blocks[g] = new_block;}
 	}
 
 	std::list<Block>::iterator splitBlock(int gene){
-
 		int g = std::abs(gene);
 
 		// Retrieve block where gene is currently located.
 		std::list<Block>::iterator b_it = genes_to_blocks[g];
 		std::list<int>::iterator g_it   = genes[g];
 
-		// Print debug info on the retrieved block.
-		std::cout << "Gene 1: " << g << "; Block 1: ";
-		(*b_it).printBlock();
-		std::cout << "Gene 1: " << (*g_it) << "\n";
-		std::cout << "Gene 2: " << (*std::next(g_it)) << "\n";
-
 		// Creates an iterator for a potential new block.
 		std::list<Block>::iterator b_new_it = b_it;
 
 		// Check if gene is not the last element in the list.
-		if (g_it != (*b_it).block.end()) {
+		if (g_it != b_it->permutationSegment.end()) {
 
-			std::cout << "Not the last element\n";
-			
 			// Move genes that appear after `gene` to a new list.
-			std::list<int> new_b;
-			new_b.splice(new_b.begin(), (*b_it).block, std::next(g_it), (*b_it).block.end());
+			std::list<int> permSegment;
+			permSegment.splice(permSegment.begin(), b_it->permutationSegment, std::next(g_it), b_it->permutationSegment.end());
 
-			std::cout << "Splice done\n";
-			(*b_it).printBlock();
 			// Create a new block containing all genes that appear after the gene specified in the input.
-			// This new block will appear next the current block in the block list.
-
-			// Updates the reference block for affected genes.
+			// This new block will appear *next* the current block in the block list.
+			createNewBlock(permSegment, b_it);
 		}
 		// Return new block.
 		return b_new_it;
 	}
 
-public:
-	// Parameterized constructor.
-	GenomeSort(const std::vector<int>& perm){
-
-		// Split permutation into blocks of size Θ(√n×log(n)).
-		int n = perm.size();
-		int blockSize = std::round(std::sqrt(n*std::log(n)));
-		std::vector<std::list<int>> permList = splitVectorIntoLists(perm, blockSize);
-
-		std::cout << "n=" << perm.size() << " -> Block size " << blockSize << "." << std::endl;
-
-		// Create a map between genes and blocks.
-		// WARNING! It assumes that (absolute) elements in the permutation [perm] are between 0 and n-1.
+	/* Method used only during construction of the object, 
+	to initialize references to genes. */
+	void initializeGenes(){
 		genes.resize(n);
-		genes_to_blocks.resize(n);
-
-		// Create a list of blocks.
-		for (int i = 0; i < permList.size(); ++i) {
-			Block& b = blocks.emplace_back(i, permList[i]); // It return a reference to the block (Block&)
-			// Get an iterator to the last block added to the list.
-			std::list<Block>::iterator new_block = std::prev(blocks.end());
-			// Update map between genes and blocks.
-			for (auto gene_it = b.block.begin(); gene_it != b.block.end(); ++gene_it) {
+		for(auto &b : blocks) {
+			for (auto gene_it = b.permutationSegment.begin(); gene_it != b.permutationSegment.end(); ++gene_it) {
 				int g = std::abs(*gene_it); // Access the first element of the iterator.
-				genes_to_blocks[g] = new_block;
 				genes[g] = gene_it;
 			}
 		}
-		blockId_max = permList.size();
-		
+	}
+
+	/* Method used only during construction of the object, 
+	to initialize list of blocks. */
+	void initializeBlocks(const std::vector<int>& perm){
+		// Split permutation into blocks.
+		int blockSize = std::round(std::sqrt(n*std::log(n)));		
+		int const numChunks	    = n / blockSize; // number of blocks.
+		int const baseChunkSize = n / numChunks;
+		int const remainder	    = n % numChunks;
+
+		// Create a list of blocks with size Θ(√n×log(n)). 
+		genes_to_blocks.resize(n);
+		int begIdx = 0;
+		for (int i=0; i<numChunks; i++) {
+			// Take a segment of the permutation.
+			int const currentChunkSize = baseChunkSize + (i < remainder ? 1 : 0);
+			std::list<int> permSegment( perm.begin() + begIdx, 
+									perm.begin() + begIdx + currentChunkSize);
+			// Make a block with the permutation segment.
+			createNewBlock(permSegment, blocks.end());
+
+			// Update position in the permutation.
+			begIdx += currentChunkSize;
+		}
+	}
+
+public:
+	// Parameterized constructor.
+	GenomeSort(const std::vector<int>& perm):n(perm.size()){
+		// Split permutation into blocks.
+		// Create a list of blocks with size Θ(√n×log(n)). 
+		initializeBlocks(perm);
+		// Initialize map of genes.
+		initializeGenes();
 		// Print blocks.
 		printBlocks();
 	}
@@ -173,7 +143,9 @@ public:
 		
 	void applyReversal(int g1, int g2){
 		splitBlock(g1);
-		//splitBlock(g2);
+		splitBlock(g2);
+		printBlocks();
+		// Reverse segment.
 	}
 	
 };
