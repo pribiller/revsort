6080c96b07e45fe58b9b5a7bc06e08195abcd094
pribiller <pribiller@gmail.com>
2025-06-16 23:31:41 +0900

    Test with templates using CRTP (Curiously Recurring Template Pattern).

--- genomePermutation.cpp
+++ genomePermutation.cpp
@@ -30,9 +30,10 @@
  * Implementation of Block class.
  *******************************************************/
 
-std::string Block::printBlock() const {
+template <typename BlockT>
+std::string BlockBase<BlockT>::printBlock() const {
 	std::string block_str = "[ ";
-	for(Gene const &g : permutationSegment) {block_str +=  (std::to_string(g.id) + "(" + std::to_string(genePosAbs(g)) + ") ");}
+	for(Gene<BlockT> const &g : permutationSegment) {block_str +=  (std::to_string(g.id) + "(" + std::to_string(genePosAbs(g)) + ") ");}
 	block_str += ("].rev=" + std::to_string(reversed));
 	return block_str;
 }
@@ -40,8 +41,8 @@ std::string Block::printBlock() const {
 /*******************************************************
  * Implementation of GenomePermutation class.
  *******************************************************/
-
-std::string GenomePermutation::printBlocks(const std::string& sep) const {
+template <typename BlockT>
+std::string GenomePermutation<BlockT>::printBlocks(const std::string& sep) const {
 	std::string blockList_str;
 	for(auto &b : blockList) {blockList_str += (sep + b.printBlock());}
 	return blockList_str;
@@ -50,10 +51,11 @@ std::string GenomePermutation::printBlocks(const std::string& sep) const {
 /* Adds a new block after the specified position. 
 WARNING! ``permSegment`` will become unusable at the end of this method.
 */
-std::list<Block>::iterator GenomePermutation::createNewBlock(const int pos, std::list<Gene>& permSegment, std::list<Block>::iterator position){
+template <typename BlockT>
+typename std::list<BlockT>::iterator GenomePermutation<BlockT>::createNewBlock(const int pos, std::list<Gene<BlockT>>& permSegment, typename std::list<BlockT>::iterator position){
 	// Create a new block containing all genes from ``permSegment``.
 	// This new block will be inserted after the block given in the input.
-	std::list<Block>::iterator new_block;
+	typename std::list<BlockT>::iterator new_block;
 	if (position != blockList.end()) {
 		blockList.emplace(std::next(position), ++maxBlockId, pos, permSegment);
 		// Get an iterator to the new block added to the list.
@@ -64,20 +66,21 @@ std::list<Block>::iterator GenomePermutation::createNewBlock(const int pos, std:
 		new_block = std::prev(blockList.end());
 	}
 	// Updates the reference block for affected genes.
-	for (Gene& g : new_block->permutationSegment) {g.block = new_block;}
+	for (Gene<BlockT>& g : new_block->permutationSegment) {g.block = new_block;}
 	return new_block;
 }
 
-
-void GenomePermutation::splitBlock(const int gene){
+// replace by pair (or tuple)
+template <typename BlockT>
+void GenomePermutation<BlockT>::splitBlock(const int gene){
 	int g = std::abs(gene);
 
 	// Retrieve block where gene is currently located.
-	std::list<Gene>::iterator  g_it = genes[g-1];
-	std::list<Block>::iterator b_it = g_it->block;
+	typename std::list<Gene<BlockT>>::iterator g_it = genes[g-1];
+	typename std::list<BlockT>::iterator b_it = g_it->block;
 
 	// Creates an iterator for a potential new block.
-	std::list<Block>::iterator new_block; // new_block = b_it;
+	typename std::list<BlockT>::iterator new_block; // new_block = b_it;
 
 	// Split depends on the block's flag ``reversed``.
 
@@ -88,11 +91,11 @@ void GenomePermutation::splitBlock(const int gene){
 		if (g_it != b_it->permutationSegment.begin()) {
 
 			// Move genes that appear before `gene` to a new list.
-			std::list<Gene> permSegment;
+			std::list<Gene<BlockT>> permSegment;
 			permSegment.splice(permSegment.begin(), b_it->permutationSegment, b_it->permutationSegment.begin(), g_it);
 			// Update relative positions of genes.
 			int relPos = 0;
-			for (Gene& g : b_it->permutationSegment) {g.pos = relPos++;}
+			for (Gene<BlockT>& g : b_it->permutationSegment) {g.pos = relPos++;}
 
 			// Create a new block containing all genes that appear before the gene specified in the input.
 			// ``permSegment`` becomes unusable after this point.
@@ -106,7 +109,7 @@ void GenomePermutation::splitBlock(const int gene){
 		if ((g_it != b_it->permutationSegment.end()) && (std::next(g_it) != b_it->permutationSegment.end())) {
 
 			// Move genes that appear after `gene` to a new list.
-			std::list<Gene> permSegment;
+			std::list<Gene<BlockT>> permSegment;
 			permSegment.splice(permSegment.begin(), b_it->permutationSegment, std::next(g_it), b_it->permutationSegment.end());
 
 			// Create a new block containing all genes that appear after the gene specified in the input.
@@ -114,37 +117,39 @@ void GenomePermutation::splitBlock(const int gene){
 			new_block = createNewBlock(b_it->pos+b_it->permutationSegment.size(), permSegment, b_it);
 		}
 	}
-	// Return new block.
-	// return new_block;
+	// Return affected blocks.
+	// return {(*b_it), (*new_block)};
 }
 
-void GenomePermutation::reverseBlock(std::list<Block>::iterator& b){
+template <typename BlockT>
+void GenomePermutation<BlockT>::reverseBlock(typename std::list<BlockT>::iterator& b){
 	// (1) Flip ``reversed`` flag of the block.
 	b->reversed = !(b->reversed);
 	// (2) Change the order of the elements.
 	b->permutationSegment.reverse();
 	// (3) Change the sign of the elements and their relative position.
 	int relPos = 0;
-	for (Gene& g : b->permutationSegment) {g.reversed = !g.reversed; g.pos = relPos++;}
+	for (Gene<BlockT>& g : b->permutationSegment) {g.reversed = !g.reversed; g.pos = relPos++;}
 }
 
 /* Block b1 = b1 + b2. Block b2: deleted. */
-void GenomePermutation::concatenateBlocks(std::list<Block>::iterator& b1, std::list<Block>::iterator& b2){
+template <typename BlockT>
+void GenomePermutation<BlockT>::concatenateBlocks(typename std::list<BlockT>::iterator& b1, typename std::list<BlockT>::iterator& b2){
 	// Update the reference block for affected genes.
-	for (Gene& g : b2->permutationSegment) {genes[g.id-1]->block = b1;}
+	for (Gene<BlockT>& g : b2->permutationSegment) {getBlock(g.id) = b1;}
 	// Flag ``reversed``: 4 cases to consider.
 	// Case 1 and Case 2: Blocks have ``reversed`` flag with the same value.
 	if(b1->reversed == b2->reversed){
 		// Case 1: b1.reversed = true; b2.reversed = true --> new_b1 = b2 + b1; new_b1.reversed = true;
 		if (b1->reversed) {
 			// Update relative positions of genes in b1.
-			for (Gene& g : b1->permutationSegment) {g.pos += b2->permutationSegment.size();}
+			for (Gene<BlockT>& g : b1->permutationSegment) {g.pos += b2->permutationSegment.size();}
 			// Move items of ``b2`` to the start of ``b1`` (emptying ``b2`` at the same time).
 			(b1->permutationSegment).splice((b1->permutationSegment).begin(), b2->permutationSegment);
 		// Case 2: b1.reversed = false; b2.reversed = false --> new_b1 = b1 + b2; new_b1.reversed = false;
 		} else {
 			// Update relative positions of genes in b2.
-			for (Gene& g : b2->permutationSegment) {g.pos += b1->permutationSegment.size();}
+			for (Gene<BlockT>& g : b2->permutationSegment) {g.pos += b1->permutationSegment.size();}
 			// Move items of ``b2`` to the end of ``b1`` (emptying ``b2`` at the same time).
 			(b1->permutationSegment).splice((b1->permutationSegment).end(), b2->permutationSegment);
 		}
@@ -156,14 +161,14 @@ void GenomePermutation::concatenateBlocks(std::list<Block>::iterator& b1, std::l
 			if(b1->permutationSegment.size() < b2->permutationSegment.size()){
 				reverseBlock(b1);
 				// Update relative positions of genes in b2.
-				for (Gene& g : b2->permutationSegment) {g.pos += b1->permutationSegment.size();}
+				for (Gene<BlockT>& g : b2->permutationSegment) {g.pos += b1->permutationSegment.size();}
 				// Move items of ``b2`` to the end of ``b1`` (emptying ``b2`` at the same time).
 				(b1->permutationSegment).splice((b1->permutationSegment).end(), b2->permutationSegment);
 			// Case 3.2: b1.reversed = true; b2.reversed = false; |b1| >= |b2| --> new_b1 = flip(b2) + b1; new_b1.reversed = true;
 			} else {
 				reverseBlock(b2);
 				// Update relative positions of genes in b1.
-				for (Gene& g : b1->permutationSegment) {g.pos += b2->permutationSegment.size();}
+				for (Gene<BlockT>& g : b1->permutationSegment) {g.pos += b2->permutationSegment.size();}
 				// Move items of ``b2`` to the start of ``b1`` (emptying ``b2`` at the same time).
 				(b1->permutationSegment).splice((b1->permutationSegment).begin(), b2->permutationSegment);
 			}
@@ -173,14 +178,14 @@ void GenomePermutation::concatenateBlocks(std::list<Block>::iterator& b1, std::l
 			if(b1->permutationSegment.size() < b2->permutationSegment.size()){
 				reverseBlock(b1);
 				// Update relative positions of genes in b1.
-				for (Gene& g : b1->permutationSegment) {g.pos += b2->permutationSegment.size();}
+				for (Gene<BlockT>& g : b1->permutationSegment) {g.pos += b2->permutationSegment.size();}
 				// Move items of ``b2`` to the start of ``b1`` (emptying ``b2`` at the same time).
 				(b1->permutationSegment).splice((b1->permutationSegment).begin(), b2->permutationSegment);
 			// Case 4.2: b1.reversed = false; b2.reversed = true; |b1| >= |b2| --> new_b1 = b1 + flip(b2); new_b1.reversed = false;
 			} else {
 				reverseBlock(b2);
 				// Update relative positions of genes in b2.
-				for (Gene& g : b2->permutationSegment) {g.pos += b1->permutationSegment.size();}
+				for (Gene<BlockT>& g : b2->permutationSegment) {g.pos += b1->permutationSegment.size();}
 				// Move items of ``b2`` to the end of ``b1`` (emptying ``b2`` at the same time).
 				(b1->permutationSegment).splice((b1->permutationSegment).end(), b2->permutationSegment);
 			}
@@ -192,17 +197,18 @@ void GenomePermutation::concatenateBlocks(std::list<Block>::iterator& b1, std::l
 
 /* Makes sure that the size of the block where gene g belongs
 lies within the interval [½×√(n×log(n)), 2×√(n×log(n))]. */
-void GenomePermutation::balanceBlock(const int gene){
+template <typename BlockT>
+void GenomePermutation<BlockT>::balanceBlock(const int gene){
 
-	std::list<Block>::iterator b = getBlock(std::abs(gene));
+	typename std::list<BlockT>::iterator b = getBlock(std::abs(gene));
 
 	// If the block is too small, then concatenate the block with a neighboring block.
 	while(b->permutationSegment.size() < minBlockSize){
 
 		std::cout << "\t [balanceBlock] Block needs to increase in size: cur.size=" <<  b->permutationSegment.size() << " / minBlockSize=" << minBlockSize << " / " << b->printBlock() << std::endl;
 
-		std::list<Block>::iterator b_prev = std::prev(b);
-		std::list<Block>::iterator b_next = std::next(b);
+		typename std::list<BlockT>::iterator b_prev = std::prev(b);
+		typename std::list<BlockT>::iterator b_next = std::next(b);
 		const int b_prev_size = (b != blockList.begin())    ? b_prev->permutationSegment.size() : (n+1);
 		const int b_next_size = (b_next != blockList.end()) ? b_next->permutationSegment.size() : (n+1);
 		// Concatenate blocks.
@@ -221,7 +227,7 @@ void GenomePermutation::balanceBlock(const int gene){
 	if(b->permutationSegment.size() > maxBlockSize) {
 		std::cout << "\t [balanceBlock] Block needs to reduce in size: cur.size=" <<  b->permutationSegment.size() << " / maxBlockSize=" << maxBlockSize << " / " << b->printBlock() << std::endl;
 
-		std::list<Gene>::iterator g_mid_it = b->permutationSegment.begin();
+		typename std::list<Gene<BlockT>>::iterator g_mid_it = b->permutationSegment.begin();
 		std::advance(g_mid_it, std::floor(b->permutationSegment.size()/2));
 		const int g_mid = g_mid_it->id;
 		splitBlock(g_mid);
@@ -230,10 +236,11 @@ void GenomePermutation::balanceBlock(const int gene){
 
 /* Method used only during construction of the object, 
 to initialize references to genes. */
-void GenomePermutation::initializeGenes(){
+template <typename BlockT>
+void GenomePermutation<BlockT>::initializeGenes(){
 	genes.resize(n);
 	for(auto &b : blockList) {
-		for (std::list<Gene>::iterator gene_it = b.permutationSegment.begin(); gene_it != b.permutationSegment.end(); ++gene_it) {
+		for (typename std::list<Gene<BlockT>>::iterator gene_it = b.permutationSegment.begin(); gene_it != b.permutationSegment.end(); ++gene_it) {
 			int g = gene_it->id; // Access the first element of the iterator.
 			genes[g-1] = gene_it;
 		}
@@ -242,7 +249,8 @@ void GenomePermutation::initializeGenes(){
 
 /* Method used only during construction of the object, 
 to initialize list of blocks. */
-void GenomePermutation::initializeBlocks(const std::vector<int>& perm){
+template <typename BlockT>
+void GenomePermutation<BlockT>::initializeBlocks(const std::vector<int>& perm){
 	// Split permutation into blocks.
 	int blockSize = std::round(std::sqrt(n*std::log(n)));		
 	int const numChunks		= n / blockSize; // number of blocks.
@@ -255,7 +263,7 @@ void GenomePermutation::initializeBlocks(const std::vector<int>& perm){
 		// Take a segment of the permutation.
 		int const currentChunkSize = baseChunkSize + (i < remainder ? 1 : 0);
 
-		std::list<Gene> permSegment;
+		std::list<Gene<BlockT>> permSegment;
 		for (int size = 0; size < currentChunkSize; ++size, ++pos) {
 			permSegment.emplace_back(std::abs(perm[pos]), size, (perm[pos] < 0));
 		}
@@ -264,4 +272,8 @@ void GenomePermutation::initializeBlocks(const std::vector<int>& perm){
 		// ``permSegment`` becomes unusable after this point.
 		createNewBlock(pos-currentChunkSize+1,permSegment, blockList.end());
 	}
+}
+
+int main(int argc, char* argv[]) {
+	std::cout << "Hello World! :-D\n";
 }
--- genomePermutation.hpp
+++ genomePermutation.hpp
@@ -1,73 +1,55 @@
+/*******************************************************
+ * One genome is represented as one permutation. 
+ * The permutation is given as a vector of ints,
+ * where each int corresponds to a gene, and its sign
+ * indicates the direction its transcription occurs.
+ * 
+ * This class stores this permutation as blocks
+ * of size Θ(√n×log(n)), and it makes sure that
+ * all blocks will always have their size between 
+ * [½×√(n×log(n)), 2×√(n×log(n))], regardless if
+ * they are split, concatenated, etc.
+ * 
+ * For more details:
+ * - Eric Tannier et al., 2007: "Advances on sorting by reversals";
+ * - Kaplan and Verbin, 2003: "Efficient Data Structures and a 
+ * New Randomized Approach for Sorting Signed Permutations by Reversals"
+ *******************************************************/
+
 #include <iostream>
 #include <list>
 #include <vector>
-#include <numeric> // iota
+#include <numeric>   // iota
 #include <cmath> 
-#include <string> // convert input args (string to int)
-#include <utility> // move
+#include <string>    // convert input args (string to int)
+#include <utility>   // move
 #include <algorithm> // find_if, reverse
-//#include <memory> // shared_ptr
-//#include <chrono>
-
-#include <boost/intrusive/rbtree.hpp>
 
 /*******************************************************
  * Auxiliary data structures.
  *******************************************************/
 
-class Block; // forward declaration
+template <typename BlockT>
+class BlockBase; // forward declaration
 
+template <typename BlockT>
 class Gene {
 public:
 	int id;  // Absolute value of the gene.
 	int pos; // Relative position inside the block.
-	bool reversed{false};             // Orientation of the gene (+ or -).
-	std::list<Block>::iterator block; // Block where gene currently is.
+	bool reversed{false};              // Orientation of the gene (+ or -).
+	typename std::list<BlockT>::iterator block; // Block where gene currently is.
 	Gene(const int id_, const int pos_, const bool sign_):id(id_),pos(pos_),reversed(sign_){
 	}
 };
 
-/* Each node corresponds to an arc v_i. */
-                    // Base hook with default options (i.e., optimized for speed).
-class Node : public boost::intrusive::set_base_hook<> {
+template <typename BlockT>
+class BlockBase {
 public:
-	const Gene& gene;
-	const Gene& gene_next;
-	int orientedTotal{0}; // the number of oriented arcs in the subtree rooted at node i.
-	bool reversed{false}; // nodes should be ordered backwards with respect to the original order, and all nodes change orientation.
-
-	Node(const Gene& g, const Gene& g_next):gene(g),gene_next(g_next){ 
-	}
-
-	// Returns the position of node i's successor in the current permutation.
-	inline int getPosNext() const;
-
-	// Returns if the arc i is oriented or not (oriented: sign of i and (i+1) are flipped).
-	inline bool getOrientation() const {
-		return (gene.reversed != gene_next.reversed);
-	}
-
-	// Overload the '<' operator.
-	friend bool operator<(const Node &a, const Node &b){
-		return a.getPosNext() < b.getPosNext();
-	}
-	// Overload the '>' operator.
-	friend bool operator>(const Node &a, const Node &b){
-		return a.getPosNext() > b.getPosNext();
-	}
-	// Overload the '==' operator.
-	friend bool operator==(const Node &a, const Node &b){
-		return a.getPosNext() == b.getPosNext();
-	}
-};
-
-class Block {
-public:
-	
-	std::list<Gene> permutationSegment;
-	bool reversed{false};                // Raised if the block should be read in the reverse order, changing the sign of the elements.
-	int pos;                             // Starting index of the permutation segment in the current permutation.
-	boost::intrusive::rbtree<Node> tree; // Balanced binary tree with segment elements as nodes.
+	int id;
+	std::list<Gene<BlockT>> permutationSegment;
+	bool reversed{false}; // Raised if the block should be read in the reverse order, changing the sign of the elements.
+	int pos;              // Starting index of the permutation segment in the current permutation.
 
 	/* p is a **rvalue reference** (&&) to a ``permutation segment`` created externally 
 	(i.e., a list of ints with type std::list<int>). The parameter ``p`` itself is a **lvalue**.
@@ -77,24 +59,77 @@ public:
 	we use instead ``permutationSegment(std::move(p))``.
 	After this operation, the parameter ``p`` becomes in an unspecified state and should no longer be used.
 	*/
-	Block(const int posBeg, std::list<Gene>& p_tmp):pos(posBeg),permutationSegment(std::move(p_tmp)){ 
+	BlockBase(const int id, const int posBeg, std::list<Gene<BlockT>>& p_tmp):id(id),pos(posBeg),permutationSegment(std::move(p_tmp)){ 
 		// Update relative position of genes in the new block.
 		int relPos = 0;
-		for(Gene &g : permutationSegment) {g.pos=relPos++;}
+		for(Gene<BlockT> &g : permutationSegment) {g.pos=relPos++;}
 	}
 	
 	/* It computes the absolute position of the gene based on its 
 	relative position and the absolute starting position of the block.
 	*/
-	inline int genePosAbs(Gene const &g) const {
+	inline int genePosAbs(Gene<BlockT> const &g) const {
 		return (reversed ? (pos + permutationSegment.size() - 1 -g.pos) : (pos + g.pos));
 	}
 
-	std::string printBlock() const {
-		std::string block_str = "[ ";
-		for(Gene const &g : permutationSegment) {block_str +=  (std::to_string(g.id) + "(" + std::to_string(genePosAbs(g)) + ") ");}
-		block_str += ("].rev=" + std::to_string(reversed));
-		return block_str;
+	std::string printBlock() const;
+
+	// TODO: updateBlock method that is called during splitTree?
+
+};
+
+/***********************************************************
+ * Genome represented as a permutation divided into blocks.
+ ***********************************************************/
+template <typename BlockT>
+class GenomePermutation {
+protected:
+	std::list<BlockT> blockList;
+	std::vector<typename std::list<Gene<BlockT>>::iterator> genes;  // references of genes (size=n genes).
+	int n; // number of genes
+		
+	int maxBlockId{0};
+
+	// Invariant: at *all* points during the sorting algorithm, 
+	// the block sizes are between [½×√(n×log(n)), 2×√(n×log(n))].
+	int minBlockSize;
+	int maxBlockSize;
+
+	/* Adds a new block after the specified position. 
+	WARNING! ``permSegment`` will become unusable at the end of this method.
+	*/
+	typename std::list<BlockT>::iterator createNewBlock(const int pos, std::list<Gene<BlockT>>& permSegment, typename std::list<BlockT>::iterator position);
+
+	void splitBlock(const int gene);
+
+	void reverseBlock(typename std::list<BlockT>::iterator& b);
+
+	/* Block b1 = b1 + b2. Block b2: deleted. */
+	void concatenateBlocks(typename std::list<BlockT>::iterator& b1, typename std::list<BlockT>::iterator& b2);
+
+	/* Makes sure that the size of the block where gene g belongs
+	lies within the interval [½×√(n×log(n)), 2×√(n×log(n))]. */
+	void balanceBlock(const int gene);
+
+	/* Method used only during construction of the object, 
+	to initialize references to genes. */
+	void initializeGenes();
+
+	/* Method used only during construction of the object, 
+	to initialize list of blocks. */
+	void initializeBlocks(const std::vector<int>& perm);
+
+	inline typename std::list<BlockT>::iterator& getBlock(const int gene){return genes[gene-1]->block;} 
+
+public:
+	// Parameterized constructor.
+	GenomePermutation(const std::vector<int>& perm):n(perm.size()),minBlockSize(std::floor(0.5*(std::sqrt(n*std::log(n))))),maxBlockSize(std::ceil(2*(std::sqrt(n*std::log(n))))){
+		// Split permutation into blocks.
+		// Create a list of blocks with size Θ(√n×log(n)). 
+		initializeBlocks(perm);
+		// Initialize map of genes.
+		initializeGenes();
 	}
 
+	std::string printBlocks(const std::string& sep=" ") const;
 };
